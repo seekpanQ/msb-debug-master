e4cb582211d8a44f145d7593f4fdcc7a24b34a6a
seekpan <lixin_qiu@163.com>
2026-09-06 10:41:19 +0800

    线上故障分析课代码

---
+++ 0_idea/src/main/java/com/mashibing/debug/idea/example/Printer.java
@@ -0,0 +1,27 @@
+package com.mashibing.debug.idea.example;
+
+public class Printer {
+
+    private boolean ready;
+
+    Printer() {
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.print1();
+        System.out.println("123");
+    }
+
+    void print1() {
+        System.out.println(ready);
+        System.out.println(ready=false);
+        print2();
+        System.out.println("print1 Printing to console");
+    }
+
+    void print2() {
+        System.out.println("print2 Printing to console");
+    }
+}
